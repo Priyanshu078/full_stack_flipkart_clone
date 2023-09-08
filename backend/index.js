1e1984b03307469b659a53caf14fe06887ef22db
Priyanshu078 <priyanshupaliwal078@gmail.com>
2023-09-08 16:29:08 +0530

    started writing backend of my flipkart clone

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res)=> {
+    res.send("Flipkart clone backend");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on http://localhost:${PORT}`);
+});
